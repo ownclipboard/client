4f4c1775f06c24326082657f0b3b4e3a19733375
trapcodeio <trapcode.io@gmail.com>
2021-12-18 23:33:22 +0100

    modified vite config

--- vite.config.ts
+++ vite.config.ts
@@ -7,12 +7,15 @@ export default defineConfig({
   plugins: [
     vue(),
     ViteEjsPlugin((config) => {
+      // Use Cdn for external scripts
       const useCdnForExternalScripts =
         config.env.VITE_APP_USE_CDN_FOR_EXTERNAL_SCRIPTS === "true" ||
         config.isProduction;
 
+      // Get App Name.
       const name = config.env.VITE_APP_NAME || "Ownclipboard Client";
 
+      // Get Config.
       return {
         config: {
           name,
